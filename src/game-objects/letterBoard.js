3552130cb21d740468b30518875a169dd1e7ebe4
Joshua <joshua@psychotechnology.com>
2021-03-20 21:32:17 +0000

    fixed row positions for letter board

--- src/game-objects/letterBoard.js
+++ src/game-objects/letterBoard.js
@@ -1,4 +1,4 @@
-import { ROW_SCALE_FACTOR_X, ROW_SCALE_FACTOR_Y, NUM_SCALE_FACTOR_X, NUM_SCALE_FACTOR_Y, NUM_W } from '../utils/constants/letterboard';
+import { ROW_SCALE_FACTOR_X, ROW_SCALE_FACTOR_Y, NUM_SCALE_FACTOR_X, NUM_SCALE_FACTOR_Y, NUM_W, NUM_H } from '../utils/constants/letterboard';
 import { WIDTH, HEIGHT } from '../';
 import { half } from '../utils/math';
 
@@ -19,16 +19,14 @@ class letterBoard extends Phaser.GameObjects.Container {
     this.middleRow = this.scene.make.image({ x: 0, y: 0, key: 'letterboard_row', scale: { x: ROW_SCALE_FACTOR_X, y: ROW_SCALE_FACTOR_Y }});
     this.middleRowNumber = this.scene.make.image({ x: 0, y: 0, key: 'letterboard_number', scale: { x: NUM_SCALE_FACTOR_X, y: NUM_SCALE_FACTOR_Y }});
 
-    // Phaser.Display.Align.In.TopCenter(this.middleRow, this.scene.add.zone(half(WIDTH), half(HEIGHT), WIDTH, HEIGHT), -half(NUM_W * NUM_SCALE_FACTOR_X), 0);
-    Phaser.Display.Align.To.BottomCenter(this.middleRow, this.upperRow, 0, 0);
+    Phaser.Display.Align.To.BottomCenter(this.middleRow, this.upperRow, 0, -(NUM_H * NUM_SCALE_FACTOR_Y));
     Phaser.Display.Align.To.RightCenter(this.middleRowNumber, this.middleRow, -1250, 0);
 
     //* Bottom row
     this.bottomRow = this.scene.make.image({ x: 0, y: 0, key: 'letterboard_row', scale: { x: ROW_SCALE_FACTOR_X, y: ROW_SCALE_FACTOR_Y }});
     this.bottomRowNumber = this.scene.make.image({ x: 0, y: 0, key: 'letterboard_number', scale: { x: NUM_SCALE_FACTOR_X, y: NUM_SCALE_FACTOR_Y }});
 
-    // Phaser.Display.Align.In.TopCenter(this.bottomRow, this.scene.add.zone(half(WIDTH), half(HEIGHT), WIDTH, HEIGHT), -half(NUM_W * NUM_SCALE_FACTOR_X), 0);
-    Phaser.Display.Align.To.BottomCenter(this.bottomRow, this.middleRow, 0, 0);
+    Phaser.Display.Align.To.BottomCenter(this.bottomRow, this.middleRow, 0, -(NUM_H * NUM_SCALE_FACTOR_Y));
     Phaser.Display.Align.To.RightCenter(this.bottomRowNumber, this.bottomRow, -1250, 0);
 
 
--- src/utils/constants/letterboard.js
+++ src/utils/constants/letterboard.js
@@ -1,4 +1,5 @@
 export const NUM_W = 352;
+export const NUM_H = 349;
 export const ROW_SCALE_FACTOR_X = 0.4;
 export const ROW_SCALE_FACTOR_Y = 0.4;
 export const NUM_SCALE_FACTOR_X = 0.4;
