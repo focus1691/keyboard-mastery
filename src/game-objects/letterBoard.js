a78c06fa3ab0c0020d5ce7d5a64e78169cdb1b32
Joshua <joshua@psychotechnology.com>
2021-03-21 00:20:01 +0000

    Adding squares inside row

--- src/game-objects/letterBoard.js
+++ src/game-objects/letterBoard.js
@@ -1,36 +1,50 @@
-import { ROW_SCALE_FACTOR_X, ROW_SCALE_FACTOR_Y, NUM_SCALE_FACTOR_X, NUM_SCALE_FACTOR_Y, NUM_W, NUM_H } from '../utils/constants/letterboard';
+import { ROW_SCALE_FACTOR_X, ROW_SCALE_FACTOR_Y, NUM_SCALE_FACTOR_X, NUM_SCALE_FACTOR_Y, NUM_W, NUM_H, SQUARE_W, SQUARE_H } from '../utils/constants/letterboard';
 import { WIDTH, HEIGHT } from '../';
 import { half } from '../utils/math';
 
 class letterBoard extends Phaser.GameObjects.Container {
   constructor(scene, config) {
     super(scene, config.x, config.y);
-    console.log(HEIGHT);
+
+    this.blocks = [];
 
     //* Top row
-    this.upperRow = this.scene.make.image({ x: 0, y: 0, key: 'letterboard_row', scale: { x: ROW_SCALE_FACTOR_X, y: ROW_SCALE_FACTOR_Y }});
-    this.upperRowNumber = this.scene.make.image({ x: 0, y: 0, key: 'letterboard_number', scale: { x: NUM_SCALE_FACTOR_X, y: NUM_SCALE_FACTOR_Y }});
+    this.upperRow = this.scene.make.image({ x: 0, y: 0, key: 'letterboard_row', scale: { x: ROW_SCALE_FACTOR_X, y: ROW_SCALE_FACTOR_Y } });
+    this.upperRowNumber = this.scene.make.image({ x: 0, y: 0, key: 'letterboard_number', scale: { x: NUM_SCALE_FACTOR_X, y: NUM_SCALE_FACTOR_Y } });
 
     Phaser.Display.Align.In.TopCenter(this.upperRow, this.scene.add.zone(half(WIDTH), half(HEIGHT), WIDTH, HEIGHT), -half(NUM_W * NUM_SCALE_FACTOR_X), 0);
     Phaser.Display.Align.To.RightCenter(this.upperRowNumber, this.upperRow, -1250, 0);
 
-
     //* Middle row
-    this.middleRow = this.scene.make.image({ x: 0, y: 0, key: 'letterboard_row', scale: { x: ROW_SCALE_FACTOR_X, y: ROW_SCALE_FACTOR_Y }});
-    this.middleRowNumber = this.scene.make.image({ x: 0, y: 0, key: 'letterboard_number', scale: { x: NUM_SCALE_FACTOR_X, y: NUM_SCALE_FACTOR_Y }});
+    this.middleRow = this.scene.make.image({ x: 0, y: 0, key: 'letterboard_row', scale: { x: ROW_SCALE_FACTOR_X, y: ROW_SCALE_FACTOR_Y } });
+    this.middleRowNumber = this.scene.make.image({ x: 0, y: 0, key: 'letterboard_number', scale: { x: NUM_SCALE_FACTOR_X, y: NUM_SCALE_FACTOR_Y } });
 
     Phaser.Display.Align.To.BottomCenter(this.middleRow, this.upperRow, 0, -(NUM_H * NUM_SCALE_FACTOR_Y));
     Phaser.Display.Align.To.RightCenter(this.middleRowNumber, this.middleRow, -1250, 0);
 
     //* Bottom row
-    this.bottomRow = this.scene.make.image({ x: 0, y: 0, key: 'letterboard_row', scale: { x: ROW_SCALE_FACTOR_X, y: ROW_SCALE_FACTOR_Y }});
-    this.bottomRowNumber = this.scene.make.image({ x: 0, y: 0, key: 'letterboard_number', scale: { x: NUM_SCALE_FACTOR_X, y: NUM_SCALE_FACTOR_Y }});
+    this.bottomRow = this.scene.make.image({ x: 0, y: 0, key: 'letterboard_row', scale: { x: ROW_SCALE_FACTOR_X, y: ROW_SCALE_FACTOR_Y } });
+    this.bottomRowNumber = this.scene.make.image({ x: 0, y: 0, key: 'letterboard_number', scale: { x: NUM_SCALE_FACTOR_X, y: NUM_SCALE_FACTOR_Y } });
 
     Phaser.Display.Align.To.BottomCenter(this.bottomRow, this.middleRow, 0, -(NUM_H * NUM_SCALE_FACTOR_Y));
     Phaser.Display.Align.To.RightCenter(this.bottomRowNumber, this.bottomRow, -1250, 0);
 
+    this.addBlock();
 
     this.scene.add.existing(this);
+  }
+  addBlock() {
+    for (let i = -9.5; i <= 9.5; i += 1.05) {
+      const block = this.scene.make.image({
+        x: 0,
+        y: 0,
+        key: 'blocks_squares',
+        frame: 'blue_square_000.png',
+        scale: { x: ROW_SCALE_FACTOR_X, y: ROW_SCALE_FACTOR_Y },
+      });
+      this.blocks.push(block);
+      Phaser.Display.Align.In.Center(block, this.middleRow, (SQUARE_W * i * ROW_SCALE_FACTOR_X), 0);
+    }
   }
 }
 
--- src/utils/constants/letterboard.js
+++ src/utils/constants/letterboard.js
@@ -4,3 +4,6 @@ export const ROW_SCALE_FACTOR_X = 0.4;
 export const ROW_SCALE_FACTOR_Y = 0.4;
 export const NUM_SCALE_FACTOR_X = 0.4;
 export const NUM_SCALE_FACTOR_Y = 0.4;
+
+export const SQUARE_W = 187;
+export const SQUARE_H = 185;
